7275cb0658ae51e68237b3c807c0bd40a8e8abf1
wdxpz <wdxpz@qq.com>
2020-06-05 22:28:08 +0800

    [all] add docker-compose.yml, move robotmaster's enviroment variables from launch.sh to docker-compose.yml

--- launch.sh
+++ launch.sh
@@ -2,10 +2,10 @@
 
 source /opt/ros/kinetic/setup.bash
 source /root/catkin_ws/devel/setup.bash
-export ROS_MASTER_URI=http://192.168.27.1:11311
-export ROS_HOSTNAME=192.168.27.1
-export TURTLEBOT3_MODEL=waffle_pi
-export TZ=Asia/Shanghai
+#export ROS_MASTER_URI=http://192.168.27.1:11311
+#export ROS_HOSTNAME=192.168.27.1
+#export TURTLEBOT3_MODEL=waffle_pi
+#export TZ=Asia/Shanghai
 
 cd /projects/robotmaster
 
